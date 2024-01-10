f5067b381708bf665585a0ab1980274ebb4f16f2
Andres Flores <andresdavid_2000@hotmail.com>
2024-01-09 20:17:32 -0600

    ModificacionDetectores1

--- DetectoresBotiquin/DetectoresBotiquin.ino
+++ DetectoresBotiquin/DetectoresBotiquin.ino
@@ -1,3 +1,7 @@
+#include <Preferences.h>
+Preferences preferences;
+preferences.begin("CalSensorPeso", false); 
+
 #define fsrpin 34
 #define fsrpin2 32
 #define Switch1 33
@@ -12,14 +16,12 @@ String idChat = "-4012404846";//ID_Chat se obtiene de telegram
 String token = "6935071361:AAHMK8o85_fqfNKv_0KiTFcOnIuotpF9XHg";
 WiFiClientSecure clientTCP;
 UniversalTelegramBot bot(token, clientTCP);
-int fsrreading; //Variable to store FSR value
-int fsrreading2; //Variable to store FSR value
 bool Detector1;
 bool Detector2;
-bool DatosAhora=false;
 int tiempoMsg = 1000;   //tiempo medio entre escaneo de mensajes
 long ultimoEscaneo;     //la última vez que se realizó el escaneo de mensajes
 void manejarMensajes(int nuevoMensajes);
+
 
 void setup() {
   Serial.begin(115200);
@@ -33,13 +35,11 @@ void setup() {
   clientTCP.setCACert(TELEGRAM_CERTIFICATE_ROOT); // Agregar certificado raíz para api.telegram.org
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
-    bot.sendMessage(idChat, "Sistema de detectores Botiquín Inteligente EN LINEA :)      !!!\n Escribe /Opciones para ver las posibles acciones", "");//Enviamos un mensaje a telegram para informar que el sistema está listo
+    bot.sendMessage(idChat, "/Bienvenida Botiquín Inteligente #xxx\n NombreRegistradoPorUsuarioParaBotiquin EN LINEA\n", "");//Enviamos un mensaje a telegram para informar que el sistema está listo
   }
 }
 
 void loop() {
-  if (DatosAhora==true)
-     ObteniendoDatos();
   if (millis() > ultimoEscaneo + tiempoMsg){
     int nuevoMensajes = bot.getUpdates(bot.last_message_received + 1);
     while (nuevoMensajes){
@@ -50,54 +50,59 @@ void loop() {
   }   
 }
 
-void ObteniendoDatos(){
-  Detector1 = !digitalRead(Switch1);
-  Detector2 = !digitalRead(Switch2);
-  String CadenaDatos = "Detector 1 = ";
-  CadenaDatos+= Detector1;
+/*void ObteniendoDatos(){
+  String CadenaDatos= "Objeto Peso 1 = "; 
+  CadenaDatos+= SensorPeso(fsrpin,1);
+  String CadenaDatos= "Objeto Peso 2 = "; 
+  CadenaDatos+= SensorPeso(fsrpin2,2);
+  DetectorObjeto3 = !digitalRead(Switch1);
+  DetectorObjeto4 = !digitalRead(Switch2);
+  CadenaDatos += "Detección Objeto 3 = ";
+  CadenaDatos+= DetectorObjeto3;
   CadenaDatos+="\n";
-  CadenaDatos+= "Detector 2 = ";
-  CadenaDatos+= Detector2;
+  CadenaDatos+= "Detección Objeto 4 = ";
+  CadenaDatos+= DetectorObjeto4;
   CadenaDatos+="\n";
-  fsrreading = analogRead(fsrpin);
-  fsrreading2 = analogRead(fsrpin2);
-  CadenaDatos+= "Objeto Peso 1 = ";
-  CadenaDatos+= fsrreading;
-  if (fsrreading < 10) {
-    CadenaDatos+= " - Sin objeto\n";
-  } else if (fsrreading < 200) {
-    CadenaDatos+= " - 25%\n";
-  } else if (fsrreading < 600) {
-    CadenaDatos+= " - 50%\n";
-  } else if (fsrreading < 900) {
-    CadenaDatos+= " - 75%\n";
-  } else if (fsrreading < 1200) {
-    CadenaDatos+= " - 100%\n";
-  } 
-  else {
-    CadenaDatos+= " - Objeto demasiado pesado, recalibra \n";
-  }
-  CadenaDatos+= "Objeto Peso 2 = ";
-  CadenaDatos+= fsrreading2;
-  if (fsrreading2 < 10) {
-    CadenaDatos+= " - Sin objeto\n";
-  } else if (fsrreading2 < 200) {
-    CadenaDatos+= " - 25%\n";
-  } else if (fsrreading2 < 600) {
-    CadenaDatos+= " - 50%\n";
-  } else if (fsrreading2 < 900) {
-    CadenaDatos+= " - 75%\n";
-  } else if (fsrreading2 < 1200) {
-    CadenaDatos+= " - 100%\n";
-  } 
-  else {
-    CadenaDatos+= " - Objeto demasiado pesado, recalibra\n";
-  }
+  CadenaDatos+="Información enviada a tu portal Web\n";
   bot.sendMessage(idChat, CadenaDatos, "");
   DatosAhora=false;
+}*/
+
+String SensorPeso(int PinSensorPeso,short NumeroSensorPeso){
+  String ObtCal="Calibracion"+NumeroSensorPeso;
+  preferences.begin("CalSensorPeso", true); 
+  short UltimaCalibracion = getShort(ObtCal, 0)
+  preferences.end();
+  
+  short LecturaAnt = analogRead(PinSensorPeso);
+  short LecturaActual=0;
+  byte Intentos=0;
+  byte Nmedicion=0;
+  short PromLecturas;
+  String CadenaPeso= fsrreading;
+  while Nmedicion<10:
+    LecturaActual = analogRead(PinSensorPeso);
+    if(((LecturaAnt+200)>LecturaActual) && (LecturaActual>LecturaAnt-200)){
+       PromLecturas+=LecturaActual
+       Nmedicion++;
+    }
+    else{
+       Intentos++;
+       if(Intentos>10)
+         LecturaAnt = analogRead(PinSensorPeso);
+    }
+  LecturaActual=PromLecturas/Nmedicion;
+  if (LecturaActual>UltimaCalibracion){
+      preferences.begin("CalSensorPeso", false);
+      preferences.putUShort(ObtCal, LecturaActual);
+      preferences.end();
+  }
+  LecturaActual=(LecturaActual/UltimaCalibracion)*100
+  CadenaPeso=LecturaActual+"%";
+  return CadenaPeso;
 }
 
-void manejarMensajes(int nuevoMensajes){
+/*void manejarMensajes(int nuevoMensajes){
   for (int i = 0; i < nuevoMensajes; i++){
     // ID de chat del solicitante
     String chat_id = String(bot.messages[i].chat_id);
@@ -108,24 +113,16 @@ void manejarMensajes(int nuevoMensajes){
     // Imprime el mensaje recibido
     String text = bot.messages[i].text;
     if (text == "/ActualizaDatosBotiquin@DetectoresBotInt_bot") {
-      DatosAhora = true;
+      ObteniendoDatos();
     }
-    if (text == "/CalibrarPeso1@DetectoresBotInt_bot") {
-      bot.sendMessage(idChat, "Solicitando Calibración PESO 1", "");
-    }
-    if (text == "/CalibrarPeso2@DetectoresBotInt_bot") {
-      bot.sendMessage(idChat, "Solicitando Calibración PESO 2", "");
-    }
-    if (text == "/Opciones@DetectoresBotInt_bot")//Nos muestra la ayuda en telegram
+    if (text == "/Bienvenida")//Nos muestra la ayuda en telegram
     {
-      String ayuda = "Bienvenido al sistema de detectores Botiquín Inteligente.\n";
-      ayuda += "Estas son tus opciones.\n\n";
-      ayuda += "/CalibrarPeso1 \n";
-      ayuda += "/CalibrarPeso2 \n";
-      ayuda += "/ActualizaDatosBotiquin \n";
-      ayuda += "/Opciones: Imprime este menú \n";
-      ayuda += "Recuerda el sistema distingue entre mayuculas y minusculas \n";
-      bot.sendMessage(idChat, ayuda, "");
+      String Bienvenida = "Botiquín Inteligente #xxx, NombreRegistradoPorUsuarioParaBotiquin, en línea\n";
+      Bienvenida += "¿Deseas actualizar tus datos del botiquin ahora??\n\n";
+      Bienvenida += "Da click a continuación: \n";
+      Bienvenida += "/ActualizaDatosBotiquin \n";
+      Bienvenida += "Recuerda el sistema distingue entre mayuculas y minusculas \n";
+      bot.sendMessage(idChat, Bienvenida, "");
     }
   }
-}
+}*/
